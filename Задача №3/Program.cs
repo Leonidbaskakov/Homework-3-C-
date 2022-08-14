2f2ba1a4bd6786af76bf45e911854d2b8359ae32
leonid <bskakaw2014@yandex.ru>
2022-08-14 19:01:01 +0300

    Домашнее задание к третему урок

--- Задача №3/Program.cs
+++ Задача №3/Program.cs
@@ -4,6 +4,6 @@ int number1 = Convert.ToInt32(Console.ReadLine());
 int count = 1;
 while (count <= number1 )
 {
-    Console.WriteLine($"{count} | {Math.Pow(count, 3)}");
+    Console.Write($"{count} , {Math.Pow(count, 3)}");
     count++;
 }
